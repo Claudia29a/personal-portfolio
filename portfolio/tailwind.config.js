d59234dee2d966dc1a99c06e6889ceda920247c8
Cretu Claudia <523382@student.fontys.nl>
2025-10-20 12:52:17 +0200

    added browser icon and name, colors and fonts

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customDarkBrown: '#632324',
+        customOrange: '#E79839',
+        customGrayLight: '#D9D9D9',
+        customBlack: '#000000',
+        customWhite: '#FFFFFF',
+        customAmber: '#FDBA52',
+        customBrownDark: '#923B27',
+      },
+      fontFamily: {
+        abhaya: ['"Abhaya Libre"', 'serif'],
+        abhayaMedium: ['"Abhaya Libre"', 'serif'], // Use font-weight classes to differentiate medium weight
+      },
+    },
   },
   plugins: [],
 }
