8314ab35c276daa799e27acbc618765e3173285f
Cretu Claudia <523382@student.fontys.nl>
2025-10-20 16:53:59 +0200

    created first page attempt 2 succsessfully

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -11,12 +11,15 @@ module.exports = {
         customGrayLight: '#D9D9D9',
         customBlack: '#000000',
         customWhite: '#FFFFFF',
-        customAmber: '#FDBA52',
+        customAmber: '#F8E0BB',
         customBrownDark: '#923B27',
       },
       fontFamily: {
         abhaya: ['"Abhaya Libre"', 'serif'],
         abhayaMedium: ['"Abhaya Libre"', 'serif'], // Use font-weight classes to differentiate medium weight
+      },
+      fontSize: {
+        xxs: '0.625rem',    // custom font size smaller than xs, for mobile
       },
     },
   },
