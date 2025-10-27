27f48cb576a06424823f9784d23b6a02db03fe2b
Cretu Claudia <523382@student.fontys.nl>
2025-10-27 10:44:26 +0100

    refactor expertise page layout and styles for improved  navigation

--- portfolio/src/app/expertise-page/expertise-page.component.ts
+++ portfolio/src/app/expertise-page/expertise-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-expertise',
     standalone: true,
+    imports: [RouterLink],
     templateUrl: './expertise-page.component.html',
     styleUrls: ['./expertise-page.component.css']
 })
