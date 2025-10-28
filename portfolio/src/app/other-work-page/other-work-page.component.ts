2e5aa277479527d9deea2e1050a962852a5c1645
Cretu Claudia <523382@student.fontys.nl>
2025-10-28 10:28:39 +0100

    Add mobile slideshow and popup functionality to the Other Work page

--- portfolio/src/app/other-work-page/other-work-page.component.ts
+++ portfolio/src/app/other-work-page/other-work-page.component.ts
@@ -10,4 +10,34 @@ import { CommonModule } from '@angular/common';
     styleUrls: ['./other-work-page.component.css']
 })
 export class OtherWorkPageComponent {
+    currentSlide: number = 0;
+    
+    slides: string[] = [
+        'assets/wildlife.jpg',
+        'assets/livewall.png',
+        'assets/breda.png',
+    ];
+
+    nextSlide() {
+        this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    }
+
+    prevSlide() {
+        this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+    }
+
+    goToSlide(index: number) {
+        this.currentSlide = index;
+    }
+
+    // Mobile popup state
+    showMobilePopup: boolean = false;
+
+    openPopup() {
+        this.showMobilePopup = true;
+    }
+
+    closePopup() {
+        this.showMobilePopup = false;
+    }
 }
